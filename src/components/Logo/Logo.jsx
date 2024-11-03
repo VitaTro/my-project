6ccfaf984bd184038c6f701bdc2c3e41b053d671
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-11-03 13:58:36 +0100

    new start

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,21 +1,19 @@
-import React from "react";
-
-import logoUrl from "./logo.png";
-import css from "./Logo.module.css";
+import css from './Logo.module.css';
+import logoUrl from './logo.png';
 
 const Logo = () => {
-    return (
-        <div className={css.container}>
-            <div className={css.header}>
-                <img src={logoUrl} alt="logo with tree and Nika" className={css.logo}/> 
-            </div>
-            <ul className={css['list-header']}>
+  return (
+    <div className={css.container}>
+      <div className={css.header}>
+        <img src={logoUrl} alt="logo with tree and Nika" className={css.logo} />
+      </div>
+      {/* <ul className={css['list-header']}>
                 <li><a href="./Catalog/Catalog.jsx"></a>Katalog</li>
                 <li><a href="./About/About.jsx"></a>O nas</li>
                 <li><a href="./Registration/Registration.jsx"></a>Rejestracja</li> 
                 <li><a href="./ShoppingBasket/ShoppingBasket.jsx"></a>Koszyk</li>
-            </ul>
-        </div>
-    )
-}
+            </ul> */}
+    </div>
+  );
+};
 export default Logo;
