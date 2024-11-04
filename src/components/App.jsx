358b49984090eb59872a8bd9a83a1b8570137595
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-11-04 07:53:42 +0100

    04112024

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
-import Logo from "./Logo/Logo";
-import Search from "./Search/Search";
-
+import { SignInPage } from 'pages/SignInPage';
+import { WelcomePage } from 'pages/WelcomePage';
+import { Route, Routes } from 'react-router-dom';
+import { About } from '../pages/About';
+import { ContactPage } from '../pages/ContactPage';
+import Navigation from './Navigation/Navigation';
+// import Search from "./Search/Search";
 
 export const App = () => {
- 
   return (
-    <>
-      < Logo />
-      <Search />
-    </>
+    <div>
+      <Navigation />
+      <Routes>
+        <Route index path="/" element={<WelcomePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/auth" element={<SignInPage />} />
+      </Routes>
+      {/* </Suspense> */}
+    </div>
   );
 };
